62d7065ac88743ed4d1df72c92df5af5ab0b3f8e
marszm <mariusz.szmer@gmail.com>
2021-10-01 23:13:11 +0200

    Initial commit

---
+++ src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsdataloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterreadsdataloaderApplication.class, args);
+    }
+
+}
