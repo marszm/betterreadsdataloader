7a677a08e40b50b2a2c69c9730198d30a2da2160
marszm <mariusz.szmer@gmail.com>
2021-10-02 19:50:52 +0200

    connection works fine

--- src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
+++ src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
@@ -1,12 +1,24 @@
 package com.example.betterreadsdataloader;
 
+import com.example.betterreadsdataloader.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Paths;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsdataloaderApplication {
     public static void main(String[] args) {
         SpringApplication.run(BetterreadsdataloaderApplication.class, args);
     }
 
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties) {
+        return cqlSessionBuilder -> cqlSessionBuilder.withCloudSecureConnectBundle(Paths.get("src/main/resources/secure-connect.zip"));
+    }
 }
