d142a865c68b6a3ddd483fd9eaf5936c74444a8d
marszm <mariusz.szmer@gmail.com>
2021-10-02 08:39:04 +0200

    added secure bundle and change props to.yml

--- src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
+++ src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BetterreadsdataloaderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BetterreadsdataloaderApplication.class, args);
     }
