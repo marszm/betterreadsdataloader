461d0ff340500831baf91ba0cc7d1f06e8ac31cb
marszm <mariusz.szmer@gmail.com>
2021-10-04 16:58:01 +0200

    can save Author entity into DB

--- src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
+++ src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
@@ -1,12 +1,16 @@
 package com.example.betterreadsdataloader;
 
+import com.example.betterreadsdataloader.author.Author;
+import com.example.betterreadsdataloader.author.AuthorRepository;
 import com.example.betterreadsdataloader.connection.DataStaxAstraProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Paths;
 
 
@@ -17,8 +21,20 @@ public class BetterreadsdataloaderApplication {
         SpringApplication.run(BetterreadsdataloaderApplication.class, args);
     }
 
+    @Autowired
+    AuthorRepository authorRepository;
+
     @Bean
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties) {
         return cqlSessionBuilder -> cqlSessionBuilder.withCloudSecureConnectBundle(Paths.get("src/main/resources/secure-connect.zip"));
+    }
+
+    @PostConstruct
+    public void run() {
+        Author author = new Author();
+        author.setId("1");
+        author.setName("Mario");
+        author.setPersonalName("0K0");
+        authorRepository.save(author);
     }
 }
