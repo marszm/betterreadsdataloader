5269bf7480d93266445cf96ec6e82f9699c165f4
marszm <mariusz.szmer@gmail.com>
2021-10-08 21:08:41 +0200

    load author data from local file into db

--- src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
+++ src/main/java/com/example/betterreadsdataloader/BetterreadsdataloaderApplication.java
@@ -8,11 +8,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 
 @SpringBootApplication
@@ -36,12 +42,41 @@ public class BetterreadsdataloaderApplication {
         return cqlSessionBuilder -> cqlSessionBuilder.withCloudSecureConnectBundle(Paths.get("src/main/resources/secure-connect.zip"));
     }
 
+    private void initAuthors(){
+        Path path = Paths.get(authorDumpLocation);
+        try {
+            Stream<String> lines = Files.lines(path);
+            lines.limit(5).forEach(line -> {
+                String jsonString = line.substring(line.indexOf("{"));
+                JSONObject jsonObject = new JSONObject();
+                try {
+                     jsonObject = new JSONObject(jsonString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Author author = new Author();
+                author.setName(jsonObject.optString("name"));
+                author.setPersonalName(jsonObject.optString("personal_name"));
+                author.setId(jsonObject.optString("key").replace("/authors/", ""));
+                authorRepository.save(author);
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initWorks(){
+
+    }
+
     @PostConstruct
     public void run() {
-        Author author = new Author();
-        author.setId("1");
-        author.setName("Mario");
-        author.setPersonalName("0K0");
-        authorRepository.save(author);
+        System.out.println("start");
+        long start = System.currentTimeMillis();
+        initAuthors();
+        long stop = System.currentTimeMillis();
+        System.out.println("stop");
+        System.out.println("time between start and stop: "+ (stop - start));
     }
 }
