964e591c751dc9ad844472c0df1c32de745a48f7
marszm <mariusz.szmer@gmail.com>
2021-11-24 11:01:05 +0100

    completed Book entity

--- src/main/java/com/example/betterreadsdataloader/book/Book.java
+++ src/main/java/com/example/betterreadsdataloader/book/Book.java
@@ -8,16 +8,35 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Table(value = "book_by_id")
 public record Book(
         @Id
         @PrimaryKeyColumn(name = "book_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
         String id,
-        @Column("author_name")
+
+        @Column("book_name")
         @CassandraType(type = CassandraType.Name.TEXT)
         String name,
+
+        @Column("book_description")
+        @CassandraType(type = CassandraType.Name.TEXT)
+        String description,
+
         @Column("personal_name")
         @CassandraType(type = CassandraType.Name.DATE)
-        LocalDate publishedDate) {
+        LocalDate publishedDate,
+
+        @Column("cover_ids")
+        @CassandraType(type = CassandraType.Name.LIST, typeArguments = CassandraType.Name.TEXT)
+        List<String> coverIds,
+
+        @Column("author_names")
+        @CassandraType(type = CassandraType.Name.LIST, typeArguments = CassandraType.Name.TEXT)
+        List<String> authorNames,
+
+        @Column("author_id")
+        @CassandraType(type = CassandraType.Name.LIST, typeArguments = CassandraType.Name.TEXT)
+        List<String> authorId) {
 }
